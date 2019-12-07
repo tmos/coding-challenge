c1a890096798d50b263dd2bf95eeb7612fc970cd
Tom Canac <contact@tomcanac.com>
2019-12-07 17:43:23 -0500

    homepage added

--- app.py
+++ app.py
@@ -2,21 +2,29 @@ from flask import Flask
 from flask_restful import Api, Resource
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 api = Api(app)
 
 # Load data from the JSON database
-with open('db.json', 'r') as file:
+with open('db/db.json', 'r') as file:
     users = json.load(file)
+
 
 class User(Resource):
     def get(self, name):
         for user in users:
-            if(name == user["buzzword"]):
+            if name == user["buzzword"]:
                 return user, 200
         return "This buzzword is so 2018", 404
-      
+
+
 api.add_resource(User, "/ninjify/<string:name>")
+
+
+@app.route("/")
+def root():
+    return app.send_static_file('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
