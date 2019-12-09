f20d5da273ca505c68b65c4efb91c984ed73e6d0
Tom Canac <contact@tomcanac.com>
2019-12-08 20:09:12 -0500

    update db generation

--- db/generate_db.py
+++ db/generate_db.py
@@ -1,31 +1,42 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 import random
 import json
+import re
 
-buzzwords_url = 'https://github.com/sindresorhus/awesome/blob/master/readme.md'
-buzzwords_page = requests.get(buzzwords_url)
-buzz_soup = BeautifulSoup(buzzwords_page.content, 'html.parser')
-buzzwords =  buzz_soup.select('article ul li > a')
+def scrap_data(url, css_selector, format_function = False):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    scrapped = soup.select(css_selector)
+    scrapped_text = []
+    
+    for i in scrapped:
+        scrapped_text.append(i.text.lower())
+    
+    if format_function:
+        return list(map(format_function, scrapped_text))
 
-ninjas_url = 'https://www.behindthename.com/names/usage/japanese'
-ninjas_page = requests.get(ninjas_url)
-ninjas_soup = BeautifulSoup(ninjas_page.content, 'html.parser')
-ninjas =  ninjas_soup.select('.browsename > span.listname > a')
+    return scrapped_text
+
+def format_data_ninja(string):
+    return re.sub(r"\([0-9]\)", "", string.lower()).strip()
+    
+buzzwords = scrap_data('https://github.com/sindresorhus/awesome/blob/master/readme.md', 'article ul li > a')
+ninjas = scrap_data('https://www.behindthename.com/names/usage/japanese', '.browsename > span.listname > a', format_data_ninja)
 
 db = []
 
 for buzzword in buzzwords:
     db.append({
-        "buzzword": buzzword.text.lower(),
+        "buzzword": buzzword,
         "first": {
-            "name": random.choice(ninjas).text.lower()
+            "name": random.choice(ninjas)
         },
         "second": {
-            "name": random.choice(ninjas).text.lower()
+            "name": random.choice(ninjas)
         },
         "third": {
-            "name": random.choice(ninjas).text.lower()
+            "name": random.choice(ninjas)
         }
     })
 
